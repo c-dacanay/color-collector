43b5af21968e4fffa93ae7d98b2dcf731103bb8e
Christina Dacanay <tina.dacanay@gmail.com>
2020-03-05 08:31:35 -0500

    Server Rename

--- server.js
+++ server.js
@@ -6,32 +6,42 @@ const content = fs.readFileSync('wikipedia_colors.json');
 let colors = JSON.parse(content);
 let randomColor = getRandomInt(colors.length);
 
-
+//this is saying all the files in public are automatically served to a browser
+//static file server
 app.use(express.static('public'));
 app.use(express.json());
 
 //ENDPOINTS
 app.get("/colors", (request, response) => {
     //shows all of the data
+    console.log('colors requested!');
     response.json(colors);
 });
+
+app.get("/random", (request, response) => {
+    console.log(setRandomName(), setRandomColor());
+    // response.json()
+})
+
+
+// app.post("/colors")
 
 // I know this should be client side but I don't know how to access the JSON from here?
 // Any communication by server must be initiated by the client && on load the page requests the API 
 
-app.get("/", (request, response) => {
-    ranName = setRandomName();
-    ranCol = setRandomColor();
-    ranHex = JSON.stringify(setRandomColor);
+// app.get("/", (request, response) => {
+//     ranName = setRandomName();
+//     ranCol = setRandomColor();
+//     ranHex = JSON.stringify(setRandomColor);
 
-    pgBackground = document.getElementById('color__div');
-    colName = document.getElementById('color__name');
-    colHex = document.getElementById('color__hex');
+//     const pgBackground = document.getElementById('color__div');
+//     const colName = document.getElementById('color__name');
+//     consst colHex = document.getElementById('color__hex');
 
-    colName.innerHTML = ranName;
-    console.log(ranName);
+//     colName.innerHTML = ranName;
+//     console.log(ranName);
 
-});
+// });
 
 app.listen(5500, () => {
     console.log("server listening at localhost 5500")
